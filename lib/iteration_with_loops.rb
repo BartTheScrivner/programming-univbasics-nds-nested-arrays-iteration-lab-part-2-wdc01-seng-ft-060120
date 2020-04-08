0139283940720ae40175ab20b9b7518d2812b679
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-08 05:57:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
 outer_results = []
 row_index = 0
 while row_index < src.count do
-  min = false
+  min = 0
   element_index = 0
   while element_index < src[row_index].count do
     if (src[row_index][element_index] < min || min === 0)
