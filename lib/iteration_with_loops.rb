66d9acdd4c86c02a83047268d43890f68b099f57
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-08 05:53:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,17 +4,19 @@ def find_min_in_nested_arrays(src)
 outer_results = []
 row_index = 0
 while row_index < src.count do
+  min = 100
   element_index = 0
   while element_index < src[row_index].count do
-    min = nil
-    if (min )
+    if (src[row_index][element_index] < min)
       min = src[row_index][element_index]
-    else (src[row_index][element_index] < min)
-      min = src[row_index][element_index]
-      outer_results.push(min)
     end
     element_index += 1
   end
+  outer_results.push(min)
   row_index += 1
 end
+end
+
+def find_greater_pair(src)
+  
 end
